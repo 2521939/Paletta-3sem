60f65d4b84ea87bc7e41d75e0147276c1abb2f01
NoL <2521466@unicesusc.edu.br>
2026-08-31 22:17:54 -0300

    Adicionado Header com Modal de Login.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import './Header.css'
+import Header from './Header'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-
+    <Header/>
     </>
+
+    
   )
 }
 
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,56 @@
+import React from 'react';
+import { useRef } from 'react';
+import './Header.css';
+import './App.css';
+import './Login.css'
+
+function Header() {
+        const dialogRef = useRef(null);
+    
+        const abrirModal = () => {
+            dialogRef.current.showModal();
+        };
+    
+        const fecharModal = () => {
+            dialogRef.current.close();
+        };
+    
+  return (
+    <>
+     <header className="cont-cabecalho">
+        <h1 className="item-cabecalho logo-cabecalho">Paletta</h1>
+        <button className="item-cabecalho login-cabecalho botaotexto" onClick={abrirModal}>Login</button>
+        <button className="item-cabecalho register-cabecalho botaotexto" id='registro'>Register</button>
+
+     </header>
+     
+     {/*Campo de Login (Modal)*/}
+<dialog ref={dialogRef} className="modal-de-login">
+    <div className="header-modal-login" >
+        <h2 className="login-header-texto">Login</h2>
+        <button className="fecharbtn" onClick={fecharModal}>X</button>
+    </div>
+
+    <div className="campos">
+        <div className="campo-nome-email">
+            <label for="username">Usuário / Email: </label>
+            <input type="text" placeholder="Nome ou email"/>
+        </div>  
+
+        <div className="campo-senha">
+            <label for="senha">Senha: </label>
+            <input type="password" placeholder="Digite sua Senha"/>
+            <div className="container-botao-entrar-teste">
+        <button className="entrar-botao">Entrar</button>
+            </div>
+        </div>
+    </div>
+</dialog>
 
 
-return {
+     
+    </>
+  );
+}
 
-};
-
-export default Header
+export default Header;
